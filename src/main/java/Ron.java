4b7f78e331acf830c87845a70a233bfb3d3c4121
salads-source <ron52086@gmail.com>
2025-01-24 09:52:32 +0800

    Add: Echo commands and exits when user enters bye

--- src/main/java/Ron.java
+++ src/main/java/Ron.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ron {
     public static String greetUser() {
         return """
@@ -5,9 +7,28 @@ public class Ron {
                 Hello! I'm Ron
                 What can I do for you?
                 ____________________________________________________________
-                Bye. Hope to see you again soon!
-                ____________________________________________________________""";
+                """;
     }
+
+    public static String echoCommand(String command) {
+        String result =  """
+                ____________________________________________________________
+                %s
+                ____________________________________________________________
+                """;
+        result = String.format(result, command);
+
+        return result;
+    }
+
+    public static String farewellUser() {
+        return """
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,5 +37,18 @@ public class Ron {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println(greetUser());
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String nextCommand = scanner.nextLine();
+            if (nextCommand.equals("bye")) {
+                break;
+            }
+            System.out.println(echoCommand(nextCommand));
+        }
+
+        System.out.println(farewellUser());
+        scanner.close();
+
     }
 }
