3eadf436d60f0260f823948ea713cfd8f4dbb0f8
salads-source <ron52086@gmail.com>
2025-01-24 09:34:53 +0800

    Add: Greet and exit message

--- src/main/java/Ron.java
+++ src/main/java/Ron.java
@@ -1,4 +1,13 @@
 public class Ron {
+    public static String greetUser() {
+        return """
+                ____________________________________________________________
+                Hello! I'm Ron
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________""";
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,6 @@ public class Ron {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(greetUser());
     }
 }
